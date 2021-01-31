07c9de55eaa6bfa2f06cdd33ac093045a8e67a88
asolodova <angel.solodova@gmail.com>
2021-01-31 21:35:12 +0300

    1. JaCoCo. Процент покрытие тестами. [#282682]

--- src/main/java/ru/job4j/collection/StringCompare.java
+++ src/main/java/ru/job4j/collection/StringCompare.java
@@ -7,9 +7,7 @@ public class StringCompare implements Comparator<String> {
     public int compare(String left, String right) {
         for (int i = 0; i < Math.min(left.length(), right.length()); i++) {
             char leftCh = left.charAt(i);
-            System.out.println(leftCh);
             char rightCh = right.charAt(i);
-            System.out.println(rightCh);
             if (leftCh != rightCh) {
                 return leftCh -  rightCh;
             }
